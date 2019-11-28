afd88d4b1caae586c7f524664d6851cd4730b31d
Ryan-Neal Mes <ryan@remotelydifferent.com>
2019-11-27 22:24:13 -0800

    Allow setting of foundationInitialAddress via environment variable

--- btrust-config.js
+++ btrust-config.js
@@ -3,7 +3,7 @@ module.exports = {
   decimals: 18,
   networks: {
     development: {
-      foundationInitialAddress: '0xc1638e6d00f2c4adc0ec8bd8d4108e1c00c53ae6'
+      foundationInitialAddress: process.env.FOUNDATION_ADDRESS || '0xc1638e6d00f2c4adc0ec8bd8d4108e1c00c53ae6'
     },
     ropsten: {
       foundationInitialAddress: '0xc2e0754cdeb7e6d0f80251e9c1a08bc2c2ede17a'
