05ec740cf251820408a262eea7f7dac880d66011
Bhushan Lodha <bhushanlodha@gmail.com>
2021-04-30 19:55:36 +0200

    Refactored proposal json Updated readme to deploy contract

--- deploy/01_deploy.js
+++ deploy/01_deploy.js
@@ -8,7 +8,7 @@ module.exports = async ({
   const {deploy} = deployments;
   const {deployer, foundationInitialAddress} = await getNamedAccounts();
   const nonce = await web3.eth.getTransactionCount(deployer)
-  const governorAlphaAddress = ethers.utils.getContractAddress({ from: deployer, nonce: nonce+1 });
+  const governorAlphaAddress = ethers.utils.getContractAddress({ from: deployer, nonce: nonce+2 });
 
   // PLEASE DON'T CHANGE ORDER OF DEPLOYMNET"
 
