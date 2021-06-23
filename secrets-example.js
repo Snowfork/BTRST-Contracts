8d937920cf9b7672e190f511a03593d666db625a
Aidan Musnitzky <musnit@gmail.com>
2021-06-23 13:12:14 +0100

    update secrets example

--- secrets-example.js
+++ secrets-example.js
@@ -2,5 +2,4 @@ module.exports = {
   infuraProjectID: '1234',
   deployerPrivateKey: '28472',
   deployerAccount: '0x77fE84B9b278F979d904F358C729363997280205',
-  deployerMnemonicRopsten: 'a b c d...',
 }
